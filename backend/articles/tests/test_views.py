2eabef448f8b3f5c50bb49627a8ed5a1cd5eac98
Pampamlela <pampamlela@gmail.com>
2025-07-22 15:59:47 +0200

    add test 2nd route

--- backend/articles/tests/test_views.py
+++ backend/articles/tests/test_views.py
@@ -6,6 +6,7 @@ from articles.models import Article
 @pytest.mark.django_db
 def test_get_posts_returns_list():
     Article.objects.create(title="Mon premier post", content="Hello world!", user_id=1)
+    Article.objects.create(title="Mon 2e post", content="Hey world!", user_id=1)
 
     client = APIClient()
     url = reverse('accueil')
@@ -15,5 +16,23 @@ def test_get_posts_returns_list():
     data = response.json()
 
     assert isinstance(data, list)
-    assert len(data) == 1
+    assert len(data) == 2
     assert data[0]['title'] == "Mon premier post"
+    assert data[1]['title'] == "Mon 2e post"
+
+
+@pytest.mark.django_db
+def test_get_article_by_id():
+    article = Article.objects.create(id=1, title="Mon premier post", content="Hello world!", user_id=2)
+
+    client = APIClient()
+    url = reverse('article_id', kwargs={"id":article.id})
+    response = client.get(url)
+
+    assert response.status_code == 200
+    data = response.json()
+
+    assert isinstance(data, object)
+    assert len(data) == 6
+    assert article.title == "Mon premier post"
+        
