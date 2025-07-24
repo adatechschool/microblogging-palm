2f87a953184b4b2e8a48811c38ba82b02d73d88d
leoV0 <leo.cigale@gmail.com>
2025-07-24 17:25:40 +0200

    add pages accueil and detail

--- backend/articles/urls.py
+++ backend/articles/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import accueil, article_id 
+from .views import accueil, article_id
+from .views import article_detail_html, accueil_html
 from .postViews import article_new
 
 urlpatterns = [
-    path('accueil', accueil, name='accueil'),
-    path('article/<int:id>/', article_id, name = "article_id"),
+    path('accueil', accueil_html, name='accueil'),
+    #path('article/<int:id>/', article_id, name = "article_id"),
+    path('article/<int:id>/', article_detail_html, name = "article_detail_html"),
     path('article/new/', article_new, name = "article_new")
 ]
--- backend/articles/views.py
+++ backend/articles/views.py
@@ -9,10 +9,10 @@ from django.shortcuts import get_object_or_404
 @api_view(['GET'])
 def accueil(request):
     articles = Article.objects.all()
-    if request.headers.get('Accept') == 'application/json':
-        serializer = ArticleSerializer(articles, many=True)
-        return Response(serializer.data)
-    return render(request, 'accueil.html', {'articles': articles})
+    #if request.headers.get('Accept') == 'application/json':
+    serializer = ArticleSerializer(articles, many=True)
+    return Response(serializer.data)
+    #return render(request, 'accueil.html', {'articles': articles})
 
 
 @api_view(['GET'])
@@ -20,3 +20,16 @@ def article_id(request, id):
     articles = get_object_or_404(Article, id = id)
     serializer = ArticleSerializer(articles)
     return Response(serializer.data)
+
+def accueil_html(request):
+    articles=Article.objects.all()
+    article_ids=[article.id for article in articles]
+    request.session["articles_affiches"]=article_ids
+    return render(request, 'accueil.html', {'articles': articles})
+
+def article_detail_html(request, id):
+    articles_affiches=request.session.get("articles_affiches",[])
+    if id not in articles_affiches:
+        return render(request,"non_autorise.html", status=403)
+    article=get_object_or_404(Article, id = id)
+    return render(request,"article_detail.html",{"article":article})
