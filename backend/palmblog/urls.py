830d5bbd13060517a256a5df3b46a3b8f61bae4c
aurianebgnl <auriane.bgnl@gmail.com>
2025-07-25 13:27:22 +0200

    add: public profile

--- backend/palmblog/urls.py
+++ backend/palmblog/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from palmblog.views import home
 from users.views.login_page import login_page, logout_user
 from users.views.signup_page import signup_page
-from users.views import profile_page, edit_profile, delete_profile 
+from users.views import profile_page, edit_profile, delete_profile, public_profile
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,8 +12,9 @@ urlpatterns = [
     path('inscription/', signup_page, name='signup'),
     path('accueil/', home, name='home'),
     path('article/', include('articles.urls')),  # inclut les routes de l'app articles
-    path('users/', include('users.urls')), #peut-être à changer par profils/ ??
+    path('users/', include('users.urls')), #peut-être à supprimer ?
     path('monprofil/', profile_page.profile_view, name='profile'),
     path('monprofil/modifier/', edit_profile.edit_profile_view, name='edit_profile'),
     path('monprofil/supprimer/', delete_profile.delete_profile_view, name='delete_profile'),
+    path('profil/<int:user_id>/', public_profile.public_profile_view, name='public_profile'),
 ]
