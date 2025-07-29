38a63710e9350daac583272379acc82dd6face7d
Pampamlela <pampamlela@gmail.com>
2025-07-29 11:12:12 +0200

    add : likes on posts

--- backend/articles/urls.py
+++ backend/articles/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import accueil, article_id
 from .views import article_detail_html, accueil_html
 from .postViews import article_new_html, delete_post, edit_post
+from .likeViews import like
 from . import views
 
 urlpatterns = [
@@ -11,4 +12,5 @@ urlpatterns = [
     path('article/newpost/', article_new_html, name = "article_new_html"),
     path('article/supprimer/<int:article_id>/', delete_post, name='delete_post'),
     path('article/modifier/<int:article_id>/', edit_post, name='edit_post'),
+    path('article/<int:article_id>/like/', like, name='like'),
 ]
