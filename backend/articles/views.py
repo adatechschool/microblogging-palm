4c57a6c4e2ef0a5548e6847bb36983a981be3449
Pampamlela <pampamlela@gmail.com>
2025-07-30 12:05:33 +0200

    add paginator

--- backend/articles/views.py
+++ backend/articles/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from .models import Article
 from .serializers import ArticleSerializer
 from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator
 
 @api_view(['GET'])
 def accueil(request):
@@ -22,9 +23,15 @@ def article_id(request, id):
 
 def accueil_html(request):
     articles=Article.objects.all() #.order_by('-created_at') #pour trier les articles du plus récent au plus vieux grace au '-' devant la valeur
+    paginator = Paginator(articles, 5) # 5 articles par page
+
+    page_number = request.GET.get('page') #pour récuper le numéro de la page dans l'URL
+    page_obj = paginator.get_page(page_number)
+
     article_ids=[article.id for article in articles]
     request.session["articles_affiches"]=article_ids
-    return render(request, 'accueil.html', {'articles': articles})
+
+    return render(request, 'accueil.html', {'page_obj': page_obj})
 
 def article_detail_html(request, id):
     articles_affiches=request.session.get("articles_affiches",[])
